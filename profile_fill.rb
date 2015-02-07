ae87f067a3e8247e57b265c068a015a5ff1657a8
kcecireyes <kcecireyes@gmail.com>
2015-02-07 00:22:14 -0500

    removed unnecessary directory, added some comments

--- profile_fill.rb
+++ profile_fill.rb
@@ -2,25 +2,25 @@ require 'rubygems'
 require 'mechanize'
 require 'logger'
 
-#Mechanize.log = Logger.new $stderr
 agent = Mechanize.new
 page = agent.get('http://google.com/')
 
+# sign into account
 sign_in = page.links_with(:text => 'Sign in')[0].click
 
 #pp sign_in
 
-#sif = sign_in.forms[0]
-#sif.field_with(:name => "Email").value = "xray.app.6"
-#puts sif.methods - Object.methods
-#puts "_________________________________________"
-sif.fields[5].value = "maxltucker@gmail.com"
-sif.fields[6].value = "M9vMv6zKzPp8"
+sif = sign_in.forms[0]
+sif.field_with(:name => "Email").value = "xray.app.6"
+puts sif.methods - Object.methods
+puts "_________________________________________"
+sif.fields[5].value = "xray.app.6"
+sif.fields[6].value = "xraymyass"
 out = agent.submit sif
 
+# search and get new results
 newpage = agent.get "http://www.google.com/"
 search_form = page.form_with :name => "f"
 search_form.field_with(:name => "q").value = "haircut"
 res= agent.submit search_form
 puts res.links
-
