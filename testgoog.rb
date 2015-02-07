f14192f189e27036c7c62439e07cdd2531aedac1
Max Tucker <mlt2158@columbia.edu>
2015-02-07 00:33:29 -0500

    Cleaned up code

--- testgoog.rb
+++ testgoog.rb
@@ -8,19 +8,16 @@ page = agent.get('http://google.com/')
 
 sign_in = page.links_with(:text => 'Sign in')[0].click
 
-#pp sign_in
+sign_in_form = sign_in.forms[0]
 
-#sif = sign_in.forms[0]
-#sif.field_with(:name => "Email").value = "xray.app.6"
-#puts sif.methods - Object.methods
-#puts "_________________________________________"
-sif.fields[5].value = "maxltucker@gmail.com"
-sif.fields[6].value = "M9vMv6zKzPp8"
-out = agent.submit sif
+sign_in_form.fields[5].value = "xray.app.6"
+sign_in_form.fields[6].value = "xraymyass"
+# Unfortunately this is just javascript
+# out = agent.submit sign_in_form
 
-newpage = agent.get "http://www.google.com/"
-search_form = page.form_with :name => "f"
+s_page = agent.get "http://www.google.com/"
+search_form = s_page.form_with :name => "f"
 search_form.field_with(:name => "q").value = "haircut"
 res= agent.submit search_form
-puts res.links
+puts res.body
 
