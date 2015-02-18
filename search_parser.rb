724c4f62b49062967cc084f2b5d6c3ca4e46416b
Max Tucker <mlt2158@columbia.edu>
2015-02-18 18:15:05 -0500

    Downgrade to firefox 34

--- search_parser.rb
+++ search_parser.rb
@@ -6,11 +6,16 @@ class SearchParser
     Capybara.default_driver = :selenium
   end
   def Query
-    Capybara.visit 'http://plus.google.com'
+    Capybara.visit 'https://google.com'
     #Capybara.page.save_screenshot 'set_location.png'
     #expect(current_path).to eq(post_comments_path(post))
+    #uri = URI.parse(current_url)
+    #"#{uri.path}?#{uri.query}".should == people_path(:search => 'name')
     puts current_url
-    puts "hey"
+    puts current_path
+    fill_in 'q', :with => 'immigration\n'
+    click_on 'signIn'
+    page.save_screenshot 'set_location.png'
   end
 end
 
