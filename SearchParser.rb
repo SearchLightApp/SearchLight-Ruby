a74eb536f4f4a37ee0f0644b2fe0b84ac1478b2f
Max Tucker <mlt2158@columbia.edu>
2015-02-20 01:41:11 -0500

    Finished Global Comparison Operator

--- SearchParser.rb
+++ SearchParser.rb
@@ -11,7 +11,7 @@ class SearchParser
 		a.to_a - b.to_a
   end
 
-	def SearchPermute(a,b)
+	def Permutation(a,b)
 		score = []
 		a.each_index do |element_index|
 			index_in_b = b.index(a[element_index])
@@ -25,7 +25,7 @@ class SearchParser
   end
 
   # return true if a and b are the same
-  def SearchComparisonPrint(a,b)
+  def ComparisonPrint(a,b)
     same = true
     score = []
     a.each_index do |element_index|
@@ -36,12 +36,68 @@ class SearchParser
           same = false
           puts sprintf("%+d", diff.to_s) + "  " + a[element_index][:txt]
         end
+      else
+        puts "NA" + "  " + a[element_index][:txt]
       end
     end
     return same
   end
 
-  def SearchPrint(a)
+  def GlobalComparison(focus_city, res_db, topic)
+    res_to_index = {}
+    focus_city_array = nil
+    res_db.each do |cityname , cityhash|
+      if cityname != focus_city
+        cityresults = cityhash[topic] # an array of hashes that look like {:txt, :url}
+        res_to_index[cityresults] ||= []
+        res_to_index[cityresults].push(cityname)
+      else
+        focus_city_array = cityhash[topic]
+        puts "COMPARE:\t"+ cityname
+        puts "TOPIC:\t"  + topic
+        puts "RESULTS:\t"+ focus_city_array.length.to_s
+      end
+    end
+    if focus_city_array.nil?
+      abort('Could not find focus city')
+    end
+    res_to_index.each do |q_results, citynames|
+      puts ""
+      any_diff = false
+      puts q_results.length.to_s + " results for:"
+      PrintCities(citynames)
+      focus_city_array.each_index do |element_index|
+        index_in_other = q_results.index(focus_city_array[element_index])
+        if index_in_other.nil?
+          puts "\tREM" + "  " + focus_city_array[element_index][:txt]
+          any_diff = true
+        else
+          diff = element_index - index_in_other
+          if diff != 0
+            puts sprintf("\t%+d ", diff.to_s) + "  " + focus_city_array[element_index][:txt]
+            any_diff = true
+          end
+        end
+      end
+      q_results.each_index do |element_index|
+        index_in_other = focus_city_array.index(q_results[element_index])
+        if index_in_other.nil?
+          puts "\tADD" + "  " + q_results[element_index][:txt]
+          any_diff = true
+        end
+      end
+      if !any_diff
+        puts "\tNo difference"
+      end
+    end
+  end
+
+  def PrintCities(cityname_array)
+    puts cityname_array
+  end
+
+
+  def PrettyPrint(a)
     a.each do |elem|
       puts elem[:txt]
       puts elem[:url]
--- TestB.rb
+++ TestB.rb
@@ -4,14 +4,21 @@ require_relative 'Results'
 s = SearchParser.new
 r = $res
 
+## TODO Store Query results in an honest-to-goodness class
+
 topics = ['Abortion', 'Immigration', 'Loans', 'Healthcare']
 cities = ['Birmingham, AL', 'Phoenix, AZ', 'San Francisco, CA', 'New York, NY', 'Birmingham, AL', 'Yarmouth, MA', 'Miami, Florida', 'El Paso, TX', 'Minneapolis, MN']
 focus_topic = topics[1]
 focus_city = cities[0]
 
-puts "COMPARE: <<" + focus_topic + ">> in " +focus_city+" to national results: \n"
-$res.each do |key, value|
-  puts "--------------------------------------------"
-  puts key # key = city, value = city's hash of topics
-  s.SearchComparisonPrint(r[focus_city][focus_topic], r[key][focus_topic]);
-end
+
+s.GlobalComparison(cities[0],r,topics[1])
+puts "+++++++++++++++++++++++++++++++++++++"
+s.GlobalComparison(cities[1],r,topics[1])
+
+#puts "COMPARE: <<" + focus_topic + ">> in " +focus_city+" to national results: \n"
+#$res.each do |key, value|
+#  puts "--------------------------------------------"
+#  puts key # key = city, value = city's hash of topics
+#  s.ComparisonPrint(r[focus_city][focus_topic], r[key][focus_topic]);
+#end
