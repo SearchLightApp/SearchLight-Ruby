e133cf42e35646c13cfc60f669e503ba70f3467a
kcecireyes <kcecireyes@gmail.com>
2015-03-11 20:35:47 -0400

    *~*~*~magic*~*~*~

---
+++ magic/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magic::Application.config.secret_key_base = '139fd5d28384a965affae38c504441af332733a60776f0414a5766652a5956af844a22ca295649232cd6ce7190fcd75e13e05111b01b00fb700e0e4a4bff19e3'
