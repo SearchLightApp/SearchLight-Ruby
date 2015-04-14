37aebcd6486f530d90edfc5e818caff2c27b7776
Charles Berret <cb2849@columbia.edu>
2015-04-14 15:45:50 -0400

    scraper

---
+++ scrape_search.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+### TEST FILE
+html = Nokogiri::HTML(open("results_kathmandu.html"))
+
+### DO SEARCH
+# query = "kathmandu"
+# page = open "http://www.google.com/search?num=100&q=" + query
+# html = Nokogiri::HTML page
+
+# puts html.css('div.rc')[1]
+
+### SCRAPE LINKS
+# html.search("cite").each do |cite| # also "cite._Rm"
+#   puts cite.inner_text
+# end
+
+### SCRAPE NEWS RESULTS (??)
+# html.search("a._Dk").each do |a|
+#   puts a.inner_text
+# end
+
+### SCRAPE RESULTS BY XPATH
+html.xpath('//h3[@class="r"]').each do |h3|
+  puts h3.content
+end
