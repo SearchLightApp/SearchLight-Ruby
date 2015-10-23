b4c514a4d1c61bde3224f977acc656c80f07d287
Max Tucker <mlt2158@columbia.edu>
2015-10-23 14:48:24 -0400

    Localizaed DB config options into config.rb

--- Searchlight.rb
+++ Searchlight.rb
@@ -3,6 +3,7 @@ require_relative 'Searcher'
 require_relative './Model/Query'
 require_relative './Model/Result'
 require_relative './Model/Ad'
+require_relative './Config'
 
 def readIntoArray(path)
 	arr = []
@@ -26,10 +27,10 @@ end
 
 def run()
 	# This file tells the program where to find the Database
-	path_to_db_config = './Model/mongoid.yml'
+	#path_to_db_config = Config.path_to_db_config
 
 	# Load the config file. Second argument specifies which configuration to use.
-	Mongoid.load!(path_to_db_config, :jumpingcrab)
+	Mongoid.load!(Config.path_to_db_config, Config.db_config_id)
 	#TODO: Set more sensible options in mongoid.yml (e.g. allow retries in case of connection failure)
 
 	# Reads the appropriate files and creates an array with them
